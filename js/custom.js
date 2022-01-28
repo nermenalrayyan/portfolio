6ce7b4f30fc7eb6b309efb60867c2c3ef1bb6d56
nermenalrayyan <n.alrayyan.work@gmail.com>
2022-01-28 15:11:02 +0400

    ...

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+// Start Close Links Menu Bar
+const toggle = document.getElementById('toggle');
+const sidebar = document.getElementById('sidebar');
+
+
+// End Close Links Menu Bar
---
+++ js/dist/custom.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Start Close Links Menu Bar
+var toggle = document.getElementById('toggle');
+var sidebar = document.getElementById('sidebar'); // End Close Links Menu Bar
